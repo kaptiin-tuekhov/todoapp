259677cfdbb7c532be00dbd6047e4fbd1cf64b18
Nikita Petukhov <nedoumok99@gmail.com>
2017-07-29 00:55:49 -0400

    refactored app component to use property initializer syntax

--- src/App.js
+++ src/App.js
@@ -5,26 +5,20 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      todos: [
-        { id: 1, name: 'Learn JSX', isComplete: true },
-        { id: 2, name: 'Build', isComplete: false },
-        { id: 3, name: 'Ship', isComplete: false }
-      ],
-      currentTodo: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
+  state = {
+    todos: [
+      { id: 1, name: 'Learn JSX', isComplete: true },
+      { id: 2, name: 'Build', isComplete: false },
+      { id: 3, name: 'Ship', isComplete: false }
+    ],
+    currentTodo: ''
   }
 
-  handleInputChange ({ target: { value } }) {
+  handleInputChange = ({ target: { value } }) => {
     this.setState(prevState => ({ ...prevState, currentTodo: value }))
   }
 
-  handleSubmit (evt) {
+  handleSubmit = evt => {
     evt.preventDefault()
     this.setState(({ currentTodo, todos }) => ({
       todos: addTodo(todos, {
@@ -37,13 +31,14 @@ class App extends Component {
     }))
   }
 
-  handleEmptySubmit (evt) {
+  handleEmptySubmit = evt => {
     evt.preventDefault()
     this.setState(prevState => ({
       errorMessage: 'Please supply a todo name',
       ...prevState
     }))
   }
+
   render () {
     const { currentTodo, todos } = this.state
     const submitHandler = currentTodo
@@ -56,7 +51,10 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className='Todo-App'>
-          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
+          {this.state.errorMessage &&
+            <span className='error'>
+              {this.state.errorMessage}
+            </span>}
           <TodoForm
             handleInputChange={this.handleInputChange}
             currentTodo={currentTodo}
